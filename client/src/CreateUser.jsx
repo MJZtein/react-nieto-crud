36dffdbdbe9ced8b67079669a5389e1732ff9c6f
Mark Nieto <nietomarkj@gmail.com>
2024-03-04 13:30:24 +0800

    first commit

---
+++ client/src/CreateUser.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+function CreateUser() {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [age, setAge] = useState();
+  const navigate = useNavigate();
+
+  const Submit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3001/createUser", { name, email, age })
+      .then((result) => {
+        console.log(result);
+        navigate("/");
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
+      <div className="w-50 bg-white rounded p-3">
+        <form onSubmit={Submit}>
+          <h2>Add User</h2>
+          <div className="mb-2">
+            <label htmlFor="name">Name</label>
+            <input
+              id="name"
+              name="name"
+              type="text"
+              placeholder="Enter Name"
+              className="form-control"
+              autoComplete="on"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="mb-2">
+            <label htmlFor="email">Email</label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              placeholder="Enter Email"
+              autoComplete="on"
+              className="form-control"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-2">
+            <label htmlFor="age">Age</label>
+            <input
+              id="age"
+              name="age"
+              type="text"
+              placeholder="Enter Age"
+              className="form-control"
+              onChange={(e) => setAge(e.target.value)}
+            />
+          </div>
+          <button className="btn btn-success">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default CreateUser;
